7cf7bbc83b9ab1b5aff5cbcb960fa761fbc72e9b
sbx_user1051 <sbx_user1051@169.254.116.89>
2023-07-15 10:18:01 +0000

    Sat Jul 15 10:18:01 UTC 2023

--- test/solution_tests/CHK/test_hello.py
+++ test/solution_tests/CHK/test_hello.py
@@ -4,5 +4,5 @@ from solutions.CHK import checkout_solution
 
 class TestCheckout():
     def test_checkout(self):
-        with pytest.raises(Exception) as e_info:
+        with pytest.raises(Exception):
             checkout_solution.checkout("")
