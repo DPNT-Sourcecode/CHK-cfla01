819df796545f2dde8c4385836e3ff33595c2b8d2
sbx_user1051 <sbx_user1051@169.254.116.89>
2023-07-15 11:01:17 +0000

    Sat Jul 15 11:01:17 UTC 2023

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -8,18 +8,23 @@ def checkout(skus):
 
 
 def get_best_price_all(
-        total_checkout_info: Dict[str, Dict],
+        checkout_counts: Dict[str, int],
+        checkout_info: Dict[str, Dict],
         ) -> int:
-    # total_checkout_info is a Dict of "item": item_info
+    # checkout_info is a Dict of "item": item_info
     # (item will be e.g. "A")
     # item_info is a dict of 
-    # {"count": 20, "price": 50, "offer": offer_info}
+    # {"price": 50, "offer": offer_info}
     # where offer_info is parsed to e.g.
     # 3A for 130 => {"multi": 3, "price": 130}
+    #
+    # checkout_counts is a Dict of "item": item_counts
     raise NotImplementedError()
     
 
-def get_best_price(item_info):
+def get_best_price(item_count, item_info):
     if not item_info["offer"]:
-        return item_info["price"] * item_info["count"]
+        return item_info["price"] * item_count
+    how_many_offers = item_count // item_info["offer"]["multi"]
+
     
