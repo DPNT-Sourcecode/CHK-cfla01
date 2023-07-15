e252b501e3d64a4c0cfe684d341ee7ce598ffd0e
sbx_user1051 <sbx_user1051@169.254.116.89>
2023-07-15 10:49:17 +0000

    Sat Jul 15 10:49:17 UTC 2023

--- test/solution_tests/CHK/test_checkout.py
+++ test/solution_tests/CHK/test_checkout.py
@@ -9,8 +9,20 @@ class TestCheckout():
 
     def test_get_best_price_all(self):
         offer_A = {"multi": 3, "price": 130}
-        info_A = {"count": , "price": 50, "offer": {}}
-        checkout_info_1
+        info_A = {"count": 4, "price": 50, "offer": offer_A}
+
+        offer_B = {"multi": 2, "price": 45}
+        info_B = {"count": 5, "price": 30, "offer": offer_B}
+
+        info_C = {"count": 5, "price": 20, "offer": {}}
+        info_D = {"count": 5, "price": 15, "offer": {}}
+
+        checkout_info = {
+            "A": info_A,
+            "B": info_B,
+            "C": info_C,
+            "D": info_D,
+        }
 
     def test_get_best_price(self):
         info_1 = {"count": 0, "price": 10, "offer": {}}
