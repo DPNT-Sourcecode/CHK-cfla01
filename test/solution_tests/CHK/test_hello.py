2dc95d30f3fc4615b6ee38244bfaf9e7dae41329
sbx_user1051 <sbx_user1051@169.254.116.89>
2023-07-15 10:15:01 +0000

    Sat Jul 15 10:15:01 UTC 2023

--- test/solution_tests/CHK/test_hello.py
+++ test/solution_tests/CHK/test_hello.py
@@ -1,8 +1,8 @@
+import pytest
 from solutions.CHK import checkout_solution
 
 
 class TestCheckout():
     def test_checkout(self):
-        assert hello_solution.hello("John") == "Hello, John!"
-        assert hello_solution.hello("Ben") == "Hello, Ben!"
-        assert hello_solution.hello("Shirley") == "Hello, Shirley!"
+        with pytest.raises(Exception) as e_info:
+            checkout_solution.checkout("")
