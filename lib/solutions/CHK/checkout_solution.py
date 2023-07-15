67197d2f67e807a0221767f5a5db47b1369c4494
sbx_user1051 <sbx_user1051@169.254.116.89>
2023-07-15 10:28:17 +0000

    Sat Jul 15 10:28:17 UTC 2023

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,6 +1,15 @@
+from typing import Dict, Tuple
 
 
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
     raise NotImplementedError()
+
+
+def get_best_price_all(
+        counts: Dict[str, int],
+        offers: Dict[str, Tuple[int, int]]
+        ) -> int:
+    # Counts is a dictionary of "item": count
+    
