6217a7415f13f7df0abdfd5601d803e39f2e9ee1
sbx_user1051 <sbx_user1051@169.254.116.89>
2023-07-15 11:27:38 +0000

    Sat Jul 15 11:27:38 UTC 2023

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -8,10 +8,24 @@ def checkout(skus):
 
 
 class Checkout:
-    def __init__(self, price_table):
+    def __init__(
+            self,
+            price_table: Dict[str, Dict]
+            ):
+        # price_table is a dictionary of
+        # "item_name": item_info
+        # (item_name will be e.g. "A")
+        # item_info is a dict of 
+        # {"price": 50, "offer": offer_info}
+        # where offer_info is parsed to e.g.
+        # 3A for 130 => {"multi": 3, "price": 130}
         self.price_table = price_table
 
-    def get_best_price(self, item_name, count):
+    def get_best_price(
+            self,
+            item_name: str,
+            count: int
+            ):
         item_info = self.price_table[item_name]
         if not item_info["offer"]:
             return item_info["price"] * count
@@ -28,14 +42,7 @@ class Checkout:
             self,
             counts: Dict[str, int],
             ) -> int:
-        # counts is a Dict of "item": counts
-        # (item will be e.g. "A")
-        # item_info is a dict of 
-        # {"price": 50, "offer": offer_info}
-        # where offer_info is parsed to e.g.
-        # 3A for 130 => {"multi": 3, "price": 130}
-        #
-        # counts is a Dict of "item": item_counts
+        # counts is a Dict of "item_name": item_counts
         best_prices = [
             self.get_best_price(name, count)
             for name, count in counts.items()
