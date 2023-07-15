388a4f8c8fa5e3fc83f006f50dabce80981d6943
sbx_user1051 <sbx_user1051@169.254.116.89>
2023-07-15 10:58:17 +0000

    Sat Jul 15 10:58:17 UTC 2023

--- test/solution_tests/CHK/test_checkout.py
+++ test/solution_tests/CHK/test_checkout.py
@@ -9,19 +9,26 @@ class TestCheckout():
 
     def test_get_best_price_all(self):
         offer_A = {"multi": 3, "price": 130}
-        info_A = {"count": 4, "price": 50, "offer": offer_A}
+        info_A = {"price": 50, "offer": offer_A}
 
         offer_B = {"multi": 2, "price": 45}
-        info_B = {"count": 5, "price": 30, "offer": offer_B}
+        info_B = {"price": 30, "offer": offer_B}
 
         info_C = {"count": 5, "price": 20, "offer": {}}
-        info_D = {"count": 5, "price": 15, "offer": {}}
+        info_D = {"price": 15, "offer": {}}
 
-        checkout_info = {
+        info = {
             "A": info_A,
             "B": info_B,
             "C": info_C,
             "D": info_D,
+        }
+
+        counts = {
+            "A": 4,
+            "B": 5,
+            "C": 5,
+            "D": 5,
         }
 
         correct_answer = (
@@ -30,19 +37,19 @@ class TestCheckout():
             + (5 * 20)
             + (5 * 15)
         )
-        assert checkout_solution.get_best_price_all(checkout_info) == correct_answer
+        assert checkout_solution.get_best_price_all(counts, info) == correct_answer
 
     def test_get_best_price(self):
-        info_1 = {"count": 0, "price": 10, "offer": {}}
-        assert checkout_solution.get_best_price(info_1) == 0
+        info_1 = {"price": 10, "offer": {}}
+        assert checkout_solution.get_best_price(0, info_1) == 0
 
-        info_2 = {"count": 10, "price": 10, "offer": {}}
-        assert checkout_solution.get_best_price(info_2) == 100
+        info_2 = {"price": 10, "offer": {}}
+        assert checkout_solution.get_best_price(10, info_2) == 100
 
-        info_3 = {"count": 10, "price": 10,
+        info_3 = {"price": 10,
                   "offer": {"multi": 2, "price": 10}}
-        assert checkout_solution.get_best_price(info_3) == 50
+        assert checkout_solution.get_best_price(10, info_3) == 50
 
-        info_4 = {"count": 11, "price": 10,
+        info_4 = {"price": 10,
                   "offer": {"multi": 2, "price": 10}}
-        assert checkout_solution.get_best_price(info_4) == 60
+        assert checkout_solution.get_best_price(11, info_4) == 60
