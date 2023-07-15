ca6139dae67ee1f6fd5871085dbe3867ce7672c9
sbx_user1051 <sbx_user1051@169.254.116.89>
2023-07-15 11:16:17 +0000

    Sat Jul 15 11:16:17 UTC 2023

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -7,42 +7,37 @@ def checkout(skus):
     raise NotImplementedError()
 
 class Checkout:
-    def __init__(price_table):
+    def __init__(self, price_table):
         self.price_table = price_table
 
+    def get_best_price(self, item_name, count):
+        if not item_info["offer"]:
+            return item_info["price"] * item_count
 
-def get_best_price(self, item_name, count):
-    if not item_info["offer"]:
-        return item_info["price"] * item_count
+        how_many_offers = item_count // item_info["offer"]["multi"]
+        offer_cost = how_many_offers * item_info["offer"]["price"]
 
-    how_many_offers = item_count // item_info["offer"]["multi"]
-    offer_cost = how_many_offers * item_info["offer"]["price"]
+        remaining_items = item_count % item_info["offer"]["multi"]
+        remaining_cost = remaining_items * item_info["price"]
 
-    remaining_items = item_count % item_info["offer"]["multi"]
-    remaining_cost = remaining_items * item_info["price"]
+        return offer_cost + remaining_cost
 
-    return offer_cost + remaining_cost
-
-
-def get_best_price_all(
-        checkout_counts: Dict[str, int],
-        checkout_info: Dict[str, Dict],
-        ) -> int:
-    # checkout_info is a Dict of "item": item_info
-    # (item will be e.g. "A")
-    # item_info is a dict of 
-    # {"price": 50, "offer": offer_info}
-    # where offer_info is parsed to e.g.
-    # 3A for 130 => {"multi": 3, "price": 130}
-    #
-    # checkout_counts is a Dict of "item": item_counts
-    best_prices = [
-        get_best_price(
-            checkout_counts[item_name],
-            checkout_info[item_name]
-            )
-        for item_name in checkout_counts.keys()
-    ]
-    return sum(best_prices)
+    def get_best_price_all(
+            self,
+            counts: Dict[str, int],
+            ) -> int:
+        # counts is a Dict of "item": counts
+        # (item will be e.g. "A")
+        # item_info is a dict of 
+        # {"price": 50, "offer": offer_info}
+        # where offer_info is parsed to e.g.
+        # 3A for 130 => {"multi": 3, "price": 130}
+        #
+        # counts is a Dict of "item": item_counts
+        best_prices = [
+            self.get_best_price(name, count)
+            for name, count in counts.items()
+        ]
+        return sum(best_prices)
 
 
